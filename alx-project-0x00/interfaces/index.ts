f16ba87eea101ad70e5c94a5c578666ea57d102e
Hanan Mohamud <hananmohamudy@gmail.com>
2025-07-19 17:43:32 +0300

    updated landing.tsx and button component

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -5,3 +5,10 @@ export interface PillProps {
 
 export type ButtonSize = "small" | "medium" | "large";
 export type ButtonShape = "rounded-sm" | "rounded-md" | "rounded-full";
+
+export interface ButtonProps {
+  title: string;
+  size?: ButtonSize;
+  shape?: ButtonShape;
+  styles?: string;
+}
